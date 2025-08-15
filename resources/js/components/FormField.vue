8ef76d16e7b55c6e7e6ff97c51bd56bae6488ae9
dmeckers <morph232@inbox.lv>
2025-08-15 11:05:27 +0300

    fix: update licenses, mix-manifest, and package dependencies; enhance FormField component
    - Removed outdated vuex license from field.js.LICENSE.txt and updated regenerator-runtime license link.
    - Updated field.js hash in mix-manifest.json to reflect recent changes.
    - Added laravel-nova dependency in package.json.
    - Refactored FormField.vue to improve error handling and component structure, including new computed properties and methods for better state management.

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,115 +1,153 @@
 <template>
-    <default-field :field="currentField" :errors="allErrors" :full-width-content="true" :show-help-text="!isReadonly && showHelpText">
-        <template #field>
-            <div
-                class="z-10 p-0 w-full form-control form-input-bordered overflow-hidden relative"
-                :class="mapErrorClasses"
-                :style="{height: currentField.mapHeight + 'px'}"
-            >
-                <point-form-field
-                    v-if="mapType === 'POINT'"
-                    ref="mapField"
-                    v-model="fieldValue"
-                    :field="currentField"
-                    :resource-id="resourceId"
-                    :resource-name="resourceName"
-                    :readonly="currentField.readonly"
-                />
+  <default-field
+    :field="currentField"
+    :errors="allErrors"
+    :full-width-content="true"
+    :show-help-text="!isReadonly && showHelpText"
+  >
+    <template #field>
+      <div
+        class="z-10 p-0 w-full form-control form-input-bordered overflow-hidden relative"
+        :class="mapErrorClasses"
+        :style="{ height: currentField.mapHeight + 'px' }"
+      >
+        <point-form-field
+          v-if="mapType === 'POINT'"
+          ref="mapField"
+          v-model="fieldValue"
+          :field="currentField"
+          :resource-id="resourceId"
+          :resource-name="resourceName"
+          :readonly="currentField.readonly"
+        />
 
-                <polygon-form-field
-                    v-else-if="mapType === 'POLYGON'"
-                    ref="mapField"
-                    v-model="fieldValue"
-                    :field="currentField"
-                    :resource-id="resourceId"
-                    :resource-name="resourceName"
-                    :readonly="currentField.readonly"
-                />
+        <polygon-form-field
+          v-else-if="mapType === 'POLYGON'"
+          ref="mapField"
+          v-model="fieldValue"
+          :field="currentField"
+          :resource-id="resourceId"
+          :resource-name="resourceName"
+          :readonly="currentField.readonly"
+        />
 
-                <multi-polygon-form-field
-                    v-else-if="mapType === 'MULTI_POLYGON'"
-                    ref="mapField"
-                    v-model="fieldValue"
-                    :field="currentField"
-                    :resource-id="resourceId"
-                    :resource-name="resourceName"
-                    :readonly="currentField.readonly"
-                />
-            </div>
+        <multi-polygon-form-field
+          v-else-if="mapType === 'MULTI_POLYGON'"
+          ref="mapField"
+          v-model="fieldValue"
+          :field="currentField"
+          :resource-id="resourceId"
+          :resource-name="resourceName"
+          :readonly="currentField.readonly"
+        />
+      </div>
 
-            <map-export
-                v-if="currentField?.capture?.enabled"
-                v-model="image"
-                :field="currentField"
-                :field-value="fieldValue"
-            />
-        </template>
-    </default-field>
+      <map-export
+        v-if="currentField?.capture?.enabled"
+        v-model="image"
+        :field="currentField"
+        :field-value="fieldValue"
+      />
+    </template>
+  </default-field>
 </template>
 
 <script>
-import {DependentFormField, HandlesValidationErrors, Errors} from 'laravel-nova'
-import PointFormField from './form-fields/PointFormField'
-import PolygonFormField from './form-fields/PolygonFormField'
-import MultiPolygonFormField from './form-fields/MultiPolygonFormField'
-import MapExport from './other/MapExport'
-
+import PointFormField from "./form-fields/PointFormField";
+import PolygonFormField from "./form-fields/PolygonFormField";
+import MultiPolygonFormField from "./form-fields/MultiPolygonFormField";
+import MapExport from "./other/MapExport";
 
 export default {
-    mixins: [DependentFormField, HandlesValidationErrors],
-    props: ['resourceName', 'resourceId', 'field'],
-    components: {
-        MapExport,
-        PolygonFormField,
-        MultiPolygonFormField,
-        PointFormField
+  props: ["resourceName", "resourceId", "field", "errors"],
+  components: {
+    MapExport,
+    PolygonFormField,
+    MultiPolygonFormField,
+    PointFormField,
+  },
+  data() {
+    return {
+      fieldValue: "",
+      image: null,
+    };
+  },
+  watch: {
+    fieldValue(value) {
+      this.emitFieldValueChange(this.currentField.attribute, value);
     },
-    data() {
-        return {
-            fieldValue: '',
-            image: null
-        }
+  },
+  computed: {
+    currentField() {
+      return this.field;
     },
-    watch: {
-        fieldValue(value) {
-            this.emitFieldValueChange(this.currentField.attribute, value)
-        }
+
+    allErrors() {
+      const attribute = this.currentField.attribute;
+      const valueKey = attribute + ".value";
+      const imageKey = attribute + ".image";
+
+      const errors = {};
+
+      errors[valueKey] = [
+        ...(this.errors && this.errors.has && this.errors.has(valueKey)
+          ? this.errors.get(valueKey)
+          : []),
+        ...(this.errors && this.errors.has && this.errors.has(imageKey)
+          ? this.errors.get(imageKey)
+          : []),
+      ];
+
+      return errors;
     },
-    computed: {
-        allErrors() {
-            const attribute = this.currentField.attribute
-            const valueKey = attribute + '.value'
-            const imageKey = attribute + '.image'
 
-            const errors = {}
-
-            errors[valueKey] = [
-                ...(this.errors.has(valueKey) ? this.errors.get(valueKey) : []),
-                ...(this.errors.has(imageKey) ? this.errors.get(imageKey) : [])
-            ]
-
-            return new Errors(errors)
-        },
-
-        mapType() {
-            return this.currentField.mapType
-        },
-
-        hasLocationError() {
-            return this.errors.has(this.currentField.attribute)
-        },
-
-        mapErrorClasses() {
-            return this.hasLocationError ? this.errorClass : ''
-        }
+    mapType() {
+      return this.currentField.mapType;
     },
-    methods: {
-        fill(formData) {
-            if (this.currentField.visible) {
-                formData.append(this.currentField.attribute + '[value]', this.fieldValue || '')
-                formData.append(this.currentField.attribute + '[image]', this.image || '')
-            }
-        }
-    }
-}
+
+    hasLocationError() {
+      return (
+        this.errors &&
+        this.errors.has &&
+        this.errors.has(this.currentField.attribute)
+      );
+    },
+
+    mapErrorClasses() {
+      return this.hasLocationError ? this.errorClass : "";
+    },
+
+    errorClass() {
+      return "form-input-border-error";
+    },
+
+    isReadonly() {
+      return this.currentField.readonly || false;
+    },
+
+    showHelpText() {
+      return (
+        this.currentField.helpText && this.currentField.helpText.length > 0
+      );
+    },
+  },
+  methods: {
+    emitFieldValueChange(attribute, value) {
+      this.$emit("field-changed");
+    },
+
+    fill(formData) {
+      if (this.currentField.visible) {
+        formData.append(
+          this.currentField.attribute + "[value]",
+          this.fieldValue || ""
+        );
+        formData.append(
+          this.currentField.attribute + "[image]",
+          this.image || ""
+        );
+      }
+    },
+  },
+};
 </script>
