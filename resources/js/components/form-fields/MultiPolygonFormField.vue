f788a97d76f986dd528cd15b1c30580a9531af97
dmeckers <morph232@inbox.lv>
2025-08-15 13:50:28 +0300

    style: improve formatting and readability in MultiPolygonFormField and PolygonFormField components

--- resources/js/components/form-fields/MultiPolygonFormField.vue
+++ resources/js/components/form-fields/MultiPolygonFormField.vue
@@ -1,203 +1,226 @@
 <template>
-    <div class="map-container">
-        <ol-map
-            ref="map"
-            @click="setDirty"
-            :load-tiles-while-animating="true"
-            :load-tiles-while-interacting="true"
-            :style="mapStyles"
-            :controls="[]"
-        >
-            <ol-view
-                @change:center="setDirty"
-                :center="center"
-                :rotation="rotation"
-                :zoom="zoom"
-                :projection="projection"
-            />
+  <div class="map-container">
+    <ol-map
+      ref="map"
+      @click="setDirty"
+      :load-tiles-while-animating="true"
+      :load-tiles-while-interacting="true"
+      :style="mapStyles"
+      :controls="[]"
+    >
+      <ol-view
+        @change:center="setDirty"
+        :center="center"
+        :rotation="rotation"
+        :zoom="zoom"
+        :projection="projection"
+      />
 
-            <ol-tile-layer>
-                <ol-source-osm :url="field.templateUrl"/>
-            </ol-tile-layer>
+      <ol-tile-layer>
+        <ol-source-osm :url="field.templateUrl" />
+      </ol-tile-layer>
 
-            <ol-vector-layer :styles="vectorStyle">
-                <ol-source-vector ref="source">
-                    <ol-interaction-modify
-                        v-if="isEditable && !exportable"
-                        @modifyend="onModifyEnd"
-                        :features="selectedFeatures"
-                    />
+      <ol-vector-layer :styles="vectorStyle">
+        <ol-source-vector ref="source">
+          <ol-interaction-modify
+            v-if="isEditable && !exportable"
+            @modifyend="onModifyEnd"
+            :features="selectedFeatures"
+          />
 
-                    <ol-interaction-draw
-                        v-if="isDrawable && !exportable"
-                        @drawend="onDrawEnd"
-                        type="Polygon"
-                        :stopClick="true"
-                        :min-points="3"
-                    />
+          <ol-interaction-draw
+            v-if="isDrawable && !exportable"
+            @drawend="onDrawEnd"
+            type="Polygon"
+            :stopClick="true"
+            :min-points="3"
+          />
 
-                    <ol-interaction-snap v-if="isEditable && !exportable"/>
-                </ol-source-vector>
+          <ol-interaction-snap v-if="isEditable && !exportable" />
+        </ol-source-vector>
 
-                <ol-style>
-                    <ol-style-stroke :color="field.style.strokeColor" :width="field.style.strokeWidth"/>
-                    <ol-style-fill :color="field.style.fillColor"/>
+        <ol-style>
+          <ol-style-stroke
+            :color="field.style.strokeColor"
+            :width="field.style.strokeWidth"
+          />
+          <ol-style-fill :color="field.style.fillColor" />
 
-                    <ol-style-circle :radius="7">
-                        <ol-style-fill color="blue"/>
-                    </ol-style-circle>
-                </ol-style>
-            </ol-vector-layer>
+          <ol-style-circle :radius="7">
+            <ol-style-fill color="blue" />
+          </ol-style-circle>
+        </ol-style>
+      </ol-vector-layer>
 
-            <ol-interaction-select
-                v-if="!exportable"
-                @select="featureSelected"
-                :condition="selectCondition"
-                :features="selectedFeatures"
-            >
-                <ol-style>
-                    <ol-style-stroke :color="field.style.strokeColor" :width="field.style.strokeWidth"/>
-                    <ol-style-fill :color="field.style.fillColor"/>
-                </ol-style>
-            </ol-interaction-select>
+      <ol-interaction-select
+        v-if="!exportable"
+        @select="featureSelected"
+        :condition="selectCondition"
+        :features="selectedFeatures"
+      >
+        <ol-style>
+          <ol-style-stroke
+            :color="field.style.strokeColor"
+            :width="field.style.strokeWidth"
+          />
+          <ol-style-fill :color="field.style.fillColor" />
+        </ol-style>
+      </ol-interaction-select>
 
-            <ol-interaction-transform
-                v-if="!exportable"
-                :condition="isTransformable"
-                :scale="field.transform.scale"
-                :rotate="field.transform.rotate"
-                :stretch="field.transform.stretch"
-            />
+      <ol-interaction-transform
+        v-if="!exportable"
+        :condition="isTransformable"
+        :scale="field.transform.scale"
+        :rotate="field.transform.rotate"
+        :stretch="field.transform.stretch"
+      />
 
-            <ol-zoom-control v-if="withZoomControl && !exportable"/>
-            <ol-zoomslider-control v-if="withZoomSlider && !exportable"/>
-            <ol-fullscreen-control v-if="withFullScreenControl && !exportable"/>
-        </ol-map>
-    </div>
+      <ol-zoom-control v-if="withZoomControl && !exportable" />
+      <ol-zoomslider-control v-if="withZoomSlider && !exportable" />
+      <ol-fullscreen-control v-if="withFullScreenControl && !exportable" />
+    </ol-map>
+  </div>
 </template>
 
 <script>
-import HasMap from '../../mixins/HasMap'
-import PolygonMixin from '../../mixins/PolygonMixin'
-import {fromLonLat, toLonLat} from 'ol/proj'
-import {GeoJSON} from 'ol/format'
-import HasSearchBox from '../../mixins/HasSearchBox'
-import HasUndoControl from '../../mixins/HasUndoControl'
-import HasClearMapControl from '../../mixins/HasClearMapControl'
-import polylabel from 'polylabel'
-import ExportsMap from '../../mixins/ExportsMap'
-
+import HasMap from "../../mixins/HasMap";
+import PolygonMixin from "../../mixins/PolygonMixin";
+import { fromLonLat, toLonLat } from "ol/proj";
+import { GeoJSON } from "ol/format";
+import HasSearchBox from "../../mixins/HasSearchBox";
+import HasUndoControl from "../../mixins/HasUndoControl";
+import HasClearMapControl from "../../mixins/HasClearMapControl";
+import polylabel from "polylabel";
+import ExportsMap from "../../mixins/ExportsMap";
 
 export default {
-    mixins: [
-        HasMap, PolygonMixin, HasSearchBox, HasUndoControl, HasClearMapControl, ExportsMap
-    ],
-    props: [
-        'field', 'readonly'
-    ],
-    expose: [
-        'initCenter', 'initZones', 'clearZones', 'capture', 'isDirty'
-    ],
-    data() {
-        return {
-            values: [],
-        }
+  mixins: [
+    HasMap,
+    PolygonMixin,
+    HasSearchBox,
+    HasUndoControl,
+    HasClearMapControl,
+    ExportsMap,
+  ],
+  props: ["field", "readonly"],
+  expose: ["initCenter", "initZones", "clearZones", "capture", "isDirty"],
+  data() {
+    return {
+      values: [],
+    };
+  },
+  methods: {
+    initCenter() {
+      this.values = [];
+      let values = [];
+
+      if (this.field.value) {
+        values = JSON.parse(this.field.value);
+      }
+
+      if (values.length) {
+        const coordinates = values.map((value) => {
+          return value[0].map((v) => fromLonLat([v[0], v[1]]));
+        });
+
+        this.center = this.initFeatures(coordinates);
+
+        coordinates.forEach((coordinate) => this.setValue([coordinate]));
+      } else {
+        this.center = fromLonLat([
+          this.field.defaultLongitude,
+          this.field.defaultLatitude,
+        ]);
+      }
     },
-    methods: {
-        initCenter() {
-            this.values = []
-            let values = []
 
-            if (this.field.value) {
-                values = JSON.parse(this.field.value)
-            }
+    initFeatures(coordinates) {
+      this.geoJsonObject.features = coordinates.map((c) => {
+        return {
+          type: "Feature",
+          geometry: {
+            type: "Polygon",
+            coordinates: [c],
+          },
+        };
+      });
 
-            if (values.length) {
-                const coordinates = values.map(value => {
-                    return value[0].map(v => fromLonLat([v[0], v[1]]))
-                })
+      this.drawIsEnabled = false;
+      this.modifyIsEnabled = true;
 
-                this.center = this.initFeatures(coordinates)
+      const center = polylabel(coordinates);
 
-                coordinates.forEach(coordinate => this.setValue([coordinate]))
-            }
-            else {
-                this.center = fromLonLat([
-                    this.field.defaultLongitude,
-                    this.field.defaultLatitude
-                ])
-            }
-        },
+      if (center[0] !== NaN) {
+        return [center[0], center[1]];
+      }
 
-        initFeatures(coordinates) {
-            this.geoJsonObject.features = coordinates.map((c) => {
-                return {
-                    type: 'Feature',
-                    geometry: {
-                        type: 'Polygon',
-                        coordinates: [c]
-                    }
-                }
-            })
+      return coordinates[0][0];
+    },
 
-            this.drawIsEnabled = false
-            this.modifyIsEnabled = true
+    initZones() {
+      const zones = new GeoJSON().readFeatures(this.geoJsonObject);
 
-            const center = polylabel(coordinates)
+      if (zones.length) {
+        zones.forEach((zone) => this.selectedFeatures.push(zone));
 
-            if (center[0] !== NaN) {
-                return [
-                    center[0],
-                    center[1]
-                ]
-            }
+        this.$refs.source.source.addFeatures(zones);
+      }
 
-            return coordinates[0][0]
-        },
+      this.fitMap();
+    },
 
-        initZones() {
-            const zones = new GeoJSON().readFeatures(this.geoJsonObject)
+    onModifyEnd() {
+      this.values = [];
+      const features = this.$refs.source.source.getFeatures();
 
-            if (zones.length) {
-                zones.forEach(zone => this.selectedFeatures.push(zone))
+      if (features.length) {
+        features.forEach((feature) => {
+          this.setValue(feature.getGeometry().getCoordinates());
+        });
+      } else {
+        this.setValue([]);
+      }
+    },
 
-                this.$refs.source.source.addFeatures(zones)
-            }
+    setValue(coordinates) {
+      this.drawIsEnabled = true;
 
-            this.fitMap()
-        },
+      if (coordinates.length) {
+        this.values.push(
+          coordinates[0]
+            .map((coordinate) => toLonLat(coordinate))
+            .map((coordinate) => [coordinate[1], coordinate[0]])
+        );
 
-        onModifyEnd() {
-            this.values = []
-            const features = this.$refs.source.source.getFeatures()
+        this.fieldValue = JSON.stringify(this.values);
+        this.setDirty();
+      } else {
+        this.fieldValue = "";
+      }
+    },
+  },
 
-            if (features.length) {
-                features.forEach((feature) => {
-                    this.setValue(feature.getGeometry().getCoordinates())
-                })
-            }
-            else {
-                this.setValue([])
-            }
-        },
+  fill(formData) {
+    console.log("FormField.fill() called!", {
+      visible: this.currentField.visible,
+      attribute: this.currentField.attribute,
+      fieldValue: this.fieldValue,
+      image: this.image,
+    });
 
-        setValue(coordinates) {
-            this.drawIsEnabled = true
+    if (this.currentField.visible) {
+      formData.append(
+        this.currentField.attribute + "[value]",
+        this.fieldValue || ""
+      );
 
-            if (coordinates.length) {
-                this.values.push(
-                    coordinates[0]
-                        .map(coordinate => toLonLat(coordinate))
-                        .map(coordinate => [coordinate[1], coordinate[0]])
-                )
-
-                this.fieldValue = JSON.stringify(this.values)
-                this.setDirty()
-            }
-            else {
-                this.fieldValue = ''
-            }
-        }
+      if (this.image) {
+        formData.append(
+          this.currentField.attribute + "[image]",
+          this.image || ""
+        );
+      }
     }
-}
+  },
+};
 </script>
--- resources/js/components/form-fields/PolygonFormField.vue
+++ resources/js/components/form-fields/PolygonFormField.vue
@@ -1,191 +1,212 @@
 <template>
-    <div class="map-container">
-        <ol-map
-            ref="map"
-            @click="setDirty"
-            :load-tiles-while-animating="true"
-            :load-tiles-while-interacting="true"
-            :style="mapStyles"
-            :controls="[]"
-        >
-            <ol-view
-                @change:center="setDirty"
-                :center="center"
-                :rotation="rotation"
-                :zoom="zoom"
-                :projection="projection"
-            />
+  <div class="map-container">
+    <ol-map
+      ref="map"
+      @click="setDirty"
+      :load-tiles-while-animating="true"
+      :load-tiles-while-interacting="true"
+      :style="mapStyles"
+      :controls="[]"
+    >
+      <ol-view
+        @change:center="setDirty"
+        :center="center"
+        :rotation="rotation"
+        :zoom="zoom"
+        :projection="projection"
+      />
 
-            <ol-tile-layer>
-                <ol-source-osm :url="field.templateUrl" />
-            </ol-tile-layer>
+      <ol-tile-layer>
+        <ol-source-osm :url="field.templateUrl" />
+      </ol-tile-layer>
 
-            <ol-vector-layer :styles="vectorStyle">
-                <ol-source-vector ref="source">
-                    <ol-interaction-modify
-                        v-if="isEditable && !exportable"
-                        @modifyend="onModifyEnd"
-                        :features="selectedFeatures"
-                    />
+      <ol-vector-layer :styles="vectorStyle">
+        <ol-source-vector ref="source">
+          <ol-interaction-modify
+            v-if="isEditable && !exportable"
+            @modifyend="onModifyEnd"
+            :features="selectedFeatures"
+          />
 
-                    <ol-interaction-draw
-                        v-if="isDrawable && !exportable"
-                        @drawend="onDrawEnd"
-                        type="Polygon"
-                        :stopClick="true"
-                        :min-points="3"
-                    />
+          <ol-interaction-draw
+            v-if="isDrawable && !exportable"
+            @drawend="onDrawEnd"
+            type="Polygon"
+            :stopClick="true"
+            :min-points="3"
+          />
 
-                    <ol-interaction-snap v-if="isEditable && !exportable" />
-                </ol-source-vector>
+          <ol-interaction-snap v-if="isEditable && !exportable" />
+        </ol-source-vector>
 
-                <ol-style>
-                    <ol-style-stroke :color="field.style.strokeColor" :width="field.style.strokeWidth" />
-                    <ol-style-fill :color="field.style.fillColor" />
+        <ol-style>
+          <ol-style-stroke
+            :color="field.style.strokeColor"
+            :width="field.style.strokeWidth"
+          />
+          <ol-style-fill :color="field.style.fillColor" />
 
-                    <ol-style-circle :radius="7">
-                        <ol-style-fill color="blue" />
-                    </ol-style-circle>
-                </ol-style>
-            </ol-vector-layer>
+          <ol-style-circle :radius="7">
+            <ol-style-fill color="blue" />
+          </ol-style-circle>
+        </ol-style>
+      </ol-vector-layer>
 
-            <ol-interaction-select
-                v-if="!exportable"
-                @select="featureSelected"
-                :condition="selectCondition"
-                :features="selectedFeatures"
-            >
-                <ol-style>
-                    <ol-style-stroke :color="field.style.strokeColor" :width="field.style.strokeWidth" />
-                    <ol-style-fill :color="field.style.fillColor" />
-                </ol-style>
-            </ol-interaction-select>
+      <ol-interaction-select
+        v-if="!exportable"
+        @select="featureSelected"
+        :condition="selectCondition"
+        :features="selectedFeatures"
+      >
+        <ol-style>
+          <ol-style-stroke
+            :color="field.style.strokeColor"
+            :width="field.style.strokeWidth"
+          />
+          <ol-style-fill :color="field.style.fillColor" />
+        </ol-style>
+      </ol-interaction-select>
 
-            <ol-interaction-transform
-                v-if="!exportable"
-                :condition="isTransformable"
-                :scale="field.transform.scale"
-                :rotate="field.transform.rotate"
-                :stretch="field.transform.stretch"
-            />
+      <ol-interaction-transform
+        v-if="!exportable"
+        :condition="isTransformable"
+        :scale="field.transform.scale"
+        :rotate="field.transform.rotate"
+        :stretch="field.transform.stretch"
+      />
 
-            <ol-zoom-control v-if="withZoomControl && !exportable" />
-            <ol-zoomslider-control v-if="withZoomSlider  && !exportable" />
-            <ol-fullscreen-control v-if="withFullScreenControl  && !exportable" />
-        </ol-map>
-    </div>
+      <ol-zoom-control v-if="withZoomControl && !exportable" />
+      <ol-zoomslider-control v-if="withZoomSlider && !exportable" />
+      <ol-fullscreen-control v-if="withFullScreenControl && !exportable" />
+    </ol-map>
+  </div>
 </template>
 
 <script>
-import {toLonLat, fromLonLat} from 'ol/proj'
-import {GeoJSON} from 'ol/format'
-import HasMap from '../../mixins/HasMap'
-import PolygonMixin from '../../mixins/PolygonMixin'
-import HasSearchBox from '../../mixins/HasSearchBox'
-import HasUndoControl from '../../mixins/HasUndoControl'
-import HasClearMapControl from '../../mixins/HasClearMapControl'
-import polylabel from 'polylabel'
-import ExportsMap from '../../mixins/ExportsMap'
-
+import { toLonLat, fromLonLat } from "ol/proj";
+import { GeoJSON } from "ol/format";
+import HasMap from "../../mixins/HasMap";
+import PolygonMixin from "../../mixins/PolygonMixin";
+import HasSearchBox from "../../mixins/HasSearchBox";
+import HasUndoControl from "../../mixins/HasUndoControl";
+import HasClearMapControl from "../../mixins/HasClearMapControl";
+import polylabel from "polylabel";
+import ExportsMap from "../../mixins/ExportsMap";
 
 export default {
-    mixins: [
-        HasMap, PolygonMixin, HasSearchBox, HasUndoControl, HasClearMapControl, ExportsMap
-    ],
-    props: [
-        'field', 'readonly'
-    ],
-    expose: [
-        'initCenter', 'initZones', 'clearZones', 'capture', 'isDirty'
-    ],
-    methods: {
-        initCenter() {
-            let value = []
+  mixins: [
+    HasMap,
+    PolygonMixin,
+    HasSearchBox,
+    HasUndoControl,
+    HasClearMapControl,
+    ExportsMap,
+  ],
+  props: ["field", "readonly"],
+  expose: ["initCenter", "initZones", "clearZones", "capture", "isDirty"],
+  methods: {
+    initCenter() {
+      let value = [];
 
-            if (this.field.value) {
-                value = JSON.parse(this.field.value)
-            }
+      if (this.field.value) {
+        value = JSON.parse(this.field.value);
+      }
 
-            if (value.length) {
-                const coordinates = [
-                    value[0].map(v => fromLonLat([v[0], v[1]]))
-                ]
+      if (value.length) {
+        const coordinates = [value[0].map((v) => fromLonLat([v[0], v[1]]))];
 
-                this.center = this.initFeatures(coordinates)
-                this.setValue(coordinates)
-            }
-            else {
-                this.center = fromLonLat([
-                    this.field.defaultLongitude,
-                    this.field.defaultLatitude
-                ])
-            }
+        this.center = this.initFeatures(coordinates);
+        this.setValue(coordinates);
+      } else {
+        this.center = fromLonLat([
+          this.field.defaultLongitude,
+          this.field.defaultLatitude,
+        ]);
+      }
+    },
+
+    initFeatures(coordinates) {
+      this.geoJsonObject.features = [
+        {
+          type: "Feature",
+          geometry: {
+            type: "Polygon",
+            coordinates,
+          },
         },
+      ];
 
-        initFeatures(coordinates) {
-            this.geoJsonObject.features = [
-                {
-                    type: 'Feature',
-                    geometry: {
-                        type: 'Polygon',
-                        coordinates
-                    }
-                }
-            ]
+      this.drawIsEnabled = false;
+      this.modifyIsEnabled = true;
 
-            this.drawIsEnabled = false
-            this.modifyIsEnabled = true
+      const center = polylabel(coordinates);
 
-            const center = polylabel(coordinates)
+      if (center[0] !== NaN) {
+        return [center[0], center[1]];
+      }
 
-            if (center[0] !== NaN){
-                return [
-                    center[0],
-                    center[1]
-                ]
-            }
+      return coordinates[0][0];
+    },
 
-            return coordinates[0][0]
-        },
+    initZones() {
+      const zones = new GeoJSON().readFeatures(this.geoJsonObject);
 
-        initZones() {
-            const zones = new GeoJSON().readFeatures(this.geoJsonObject)
+      if (zones.length) {
+        this.selectedFeatures.push(zones[0]);
 
-            if (zones.length) {
-                this.selectedFeatures.push(zones[0])
+        this.$refs.source.source.addFeatures(zones);
+      }
 
-                this.$refs.source.source.addFeatures(zones)
-            }
+      this.fitMap();
+    },
 
-            this.fitMap()
-        },
+    onModifyEnd() {
+      const features = this.$refs.source.source.getFeatures();
 
-        onModifyEnd() {
-            const features = this.$refs.source.source.getFeatures()
+      if (features.length) {
+        const geometry = features[0].getGeometry();
 
-            if (features.length) {
-                const geometry = features[0].getGeometry()
+        this.setValue(geometry.getCoordinates());
+      } else {
+        this.setValue([]);
+      }
+    },
 
-                this.setValue(geometry.getCoordinates())
-            }
-            else {
-                this.setValue([])
-            }
-        },
+    fill(formData) {
+      console.log("FormField.fill() called!", {
+        visible: this.currentField.visible,
+        attribute: this.currentField.attribute,
+        fieldValue: this.fieldValue,
+        image: this.image,
+      });
 
-        setValue(coordinates) {
-            if (coordinates.length) {
-                coordinates = coordinates[0]
-                    .map(coordinate => toLonLat(coordinate))
-                    .map(coordinate => [coordinate[1], coordinate[0]])
+      if (this.currentField.visible) {
+        formData.append(
+          this.currentField.attribute + "[value]",
+          this.fieldValue || ""
+        );
 
-                this.fieldValue = JSON.stringify(coordinates)
-                this.setDirty()
-            }
-            else {
-                this.fieldValue = ''
-            }
+        if (this.image) {
+          formData.append(
+            this.currentField.attribute + "[image]",
+            this.image || ""
+          );
         }
-    }
-}
+      }
+    },
+
+    setValue(coordinates) {
+      if (coordinates.length) {
+        coordinates = coordinates[0]
+          .map((coordinate) => toLonLat(coordinate))
+          .map((coordinate) => [coordinate[1], coordinate[0]]);
+
+        this.fieldValue = JSON.stringify(coordinates);
+        this.setDirty();
+      } else {
+        this.fieldValue = "";
+      }
+    },
+  },
+};
 </script>
